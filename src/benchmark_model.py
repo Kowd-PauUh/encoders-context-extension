d39acc3939970b421e2f919161205b7d3f11045f
Ivan <kowd.pauuh@gmail.com>
2025-04-30 18:00:50 +0000

    Refined example. Added LongBench tasks

--- src/benchmark_model.py
+++ src/benchmark_model.py
@@ -2,7 +2,7 @@
 Script for model benchmarking. CLI is enabled.
 
 Example:
->>> python3 src/benchmark_model.py --model_name_or_path="idanylenko/e5-large-v2-ctx1024"
+>>> PYTHONPATH=$(pwd):$PATH python3 src/benchmark_model.py --model_name_or_path="idanylenko/e5-large-v2-ctx1024"
 """
 
 import torch
@@ -10,13 +10,15 @@ from sentence_transformers import SentenceTransformer
 import mteb
 from fire import Fire
 
+from src.longbench_tasks import LONGBENCH_TASKS
+
 
 TASK_LIST = [
     'LEMBSummScreenFDRetrieval',
     'LEMBQMSumRetrieval',
     'LEMBWikimQARetrieval',
     'LEMBNarrativeQARetrieval'
-]
+] + list(LONGBENCH_TASKS.keys())
 
 
 def benchmark_model(
@@ -29,8 +31,12 @@ def benchmark_model(
     device = model_kwargs.pop('device', 'cuda' if torch.cuda.is_available() else 'cpu')
     model = SentenceTransformer(model_name_or_path, device=device, **model_kwargs)
 
+    # prepare tasks
+    mteb_tasks = mteb.get_tasks(tasks=[t for t in tasks if t not in LONGBENCH_TASKS])
+    longbench_tasks = [LONGBENCH_TASKS[t] for t in tasks if t in LONGBENCH_TASKS]
+    tasks = longbench_tasks + list(mteb_tasks)
+
     # run the evaluation
-    tasks = mteb.get_tasks(tasks=tasks)
     evaluation = mteb.MTEB(tasks=tasks)
     results = evaluation.run(model)
 
