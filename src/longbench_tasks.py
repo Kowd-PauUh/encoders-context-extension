cd17113e4ea237985ec2f4f87fdad61c0553f721
Ivan <kowd.pauuh@gmail.com>
2025-04-30 18:00:22 +0000

    Added MTEB-compatible LongBench tasks

---
+++ src/longbench_tasks.py
@@ -0,0 +1,70 @@
+import mteb
+from mteb.abstasks.AbsTaskRetrieval import AbsTaskRetrieval
+from mteb.abstasks.TaskMetadata import TaskMetadata
+from datasets import load_dataset
+
+
+LONGBENCH_DATASETS = [
+    "hotpotqa", "2wikimqa", "musique", 
+    "dureader", "narrativeqa", "qasper",
+    "multifieldqa_en", "multifieldqa_zh", 
+    "gov_report", "qmsum", "vcsum", "trec", 
+    "nq", "triviaqa", "lsht", "passage_count",
+    "passage_retrieval_en", "passage_retrieval_zh", 
+    "lcc",
+]
+
+
+class BaseLongBenchTask(AbsTaskRetrieval):
+    metadata = TaskMetadata(
+        name='',
+        description='',
+        reference='http://example.com',
+        type='Retrieval',
+        category='p2p',
+        modalities=['text'],
+        eval_splits=['test'],
+        eval_langs=['eng-ENG'],
+        main_score='ndcg_at_10',
+        dataset={
+            "path": "",
+            "revision": "",
+        }
+    )
+    
+    def __init__(self, *args, **kwargs):
+        self.data_loaded = False
+        self.metadata.name = self.__class__.__name__
+        super().__init__()
+
+        
+    def load_data(self):
+        data = load_dataset(
+            'THUDM/LongBench',
+            self.__class__.__name__,
+            split='test'
+        )
+
+        queries = data['input']
+        passages = data['context']
+
+        self.queries = {f'qid{i}': q for i, q in enumerate(queries)}
+        self.corpus = {f'pid{i}': {'text': p} for i, p in enumerate(passages)}
+        self.relevant_docs = {f'qid{i}': {f'pid{i}'} for i in range(len(queries))}
+        self.data_loaded = True
+
+
+# create mteb retrieval tasks from base task
+TASK_CLASSES = {}
+for task_name in LONGBENCH_DATASETS:
+    TASK_CLASSES[task_name] = type(
+        task_name,
+        (BaseLongBenchTask,),
+        {
+            '__name__': task_name,
+            '__init__': (lambda task_name=task_name: 
+                         lambda self, *args, **kwargs: 
+                         super(TASK_CLASSES[task_name], self).__init__(*args, **kwargs))()
+        }
+    )
+LONGBENCH_TASKS = {k: v() for k, v in TASK_CLASSES.items()}
